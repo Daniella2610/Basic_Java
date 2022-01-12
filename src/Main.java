1c48fe633209b7de0c11d79f6217dd53717e136e
Daniella Clemmensen <2610daniella@gmail.com>
2022-01-12 17:59:59 +0100

    Basic

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        String test ="bla";
+        double FD = 2.2;
+        System.out.println("Update"+" "+ FD+ " "+ test);
+        Scanner input = new Scanner(System.in);
+        FD = input.nextDouble();
+        System.out.println("faf"+ FD);
+    }
 }
